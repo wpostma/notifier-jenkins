ea640c9e86c359aeb94532b39ef5fefb6567ca71
v.renjith <v.renjith@gmail.com>
2011-12-15 01:43:30 +0000

    First release

---
+++ JenkinsClient/Settings.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace HudsonClient
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.saveCloseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertyGridUser = new System.Windows.Forms.PropertyGrid();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageUser = new System.Windows.Forms.TabPage();
+            this.tabPageAdvanced = new System.Windows.Forms.TabPage();
+            this.propertyGridAdvanced = new System.Windows.Forms.PropertyGrid();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabPageUser.SuspendLayout();
+            this.tabPageAdvanced.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveCloseToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(483, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // saveCloseToolStripMenuItem
+            // 
+            this.saveCloseToolStripMenuItem.Name = "saveCloseToolStripMenuItem";
+            this.saveCloseToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.saveCloseToolStripMenuItem.Text = "&Save \'n\' Close";
+            this.saveCloseToolStripMenuItem.Click += new System.EventHandler(this.saveCloseToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.closeToolStripMenuItem.Text = "&Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // propertyGridUser
+            // 
+            this.propertyGridUser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGridUser.Location = new System.Drawing.Point(3, 3);
+            this.propertyGridUser.Name = "propertyGridUser";
+            this.propertyGridUser.Size = new System.Drawing.Size(469, 325);
+            this.propertyGridUser.TabIndex = 1;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPageUser);
+            this.tabControl.Controls.Add(this.tabPageAdvanced);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 24);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(483, 357);
+            this.tabControl.TabIndex = 2;
+            this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+            // 
+            // tabPageUser
+            // 
+            this.tabPageUser.Controls.Add(this.propertyGridUser);
+            this.tabPageUser.Location = new System.Drawing.Point(4, 22);
+            this.tabPageUser.Name = "tabPageUser";
+            this.tabPageUser.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageUser.Size = new System.Drawing.Size(475, 331);
+            this.tabPageUser.TabIndex = 0;
+            this.tabPageUser.Text = "User Settings";
+            this.tabPageUser.UseVisualStyleBackColor = true;
+            // 
+            // tabPageAdvanced
+            // 
+            this.tabPageAdvanced.Controls.Add(this.splitContainer);
+            this.tabPageAdvanced.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAdvanced.Name = "tabPageAdvanced";
+            this.tabPageAdvanced.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAdvanced.Size = new System.Drawing.Size(475, 331);
+            this.tabPageAdvanced.TabIndex = 1;
+            this.tabPageAdvanced.Text = "Advanced Settings";
+            this.tabPageAdvanced.UseVisualStyleBackColor = true;
+            // 
+            // propertyGridAdvanced
+            // 
+            this.propertyGridAdvanced.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGridAdvanced.Location = new System.Drawing.Point(0, 0);
+            this.propertyGridAdvanced.Name = "propertyGridAdvanced";
+            this.propertyGridAdvanced.Size = new System.Drawing.Size(469, 294);
+            this.propertyGridAdvanced.TabIndex = 0;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.propertyGridAdvanced);
+            this.splitContainer.Size = new System.Drawing.Size(469, 325);
+            this.splitContainer.SplitterDistance = 27;
+            this.splitContainer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Orange;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Change these settings at your own risk.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 381);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabPageUser.ResumeLayout(false);
+            this.tabPageAdvanced.ResumeLayout(false);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem saveCloseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.PropertyGrid propertyGridUser;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageUser;
+        private System.Windows.Forms.TabPage tabPageAdvanced;
+        private System.Windows.Forms.PropertyGrid propertyGridAdvanced;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ JenkinsClient/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HudsonClient
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void saveCloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            AdvancedSettings.Default.Save();
+            Close();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reload();
+            AdvancedSettings.Default.Reload();
+            propertyGridUser.SelectedObject = Properties.Settings.Default;
+            propertyGridAdvanced.SelectedObject = AdvancedSettings.Default;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
